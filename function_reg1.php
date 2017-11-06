666d306118d7fb04c58b425e07c268431131ef00
connect30 <avolovodovskii@gmail.com>
2017-11-06 22:03:00 +0200

    06.10.2017_HW_14

--- function_reg1.php
+++ function_reg1.php
@@ -11,18 +11,33 @@ $str = '57abc';
  
  preg_match($reg1, $str1, $m);
  foreach ($m as $value) {
-     echo '<br />'.$value;
+   //  echo '<br />'.$value;
     
 }
-     
-// preg_match('@^(?:http://)?([^/]+)@',
-//    "http://www.php.net/index.html", $matches);
-//// echo $matches[1];
-$reg = '#^\w...[c$]#';
-$str = '57abd';
- echo preg_match($reg, $str, $m);
- print_r($m);
- 
- preg_match('@^(?:http://)?([^/]+)@',
-    "http://www.php.net/index.html", $matches);
-// echo $matches[1];
+
+$str2 = 'Внизу страницы www.mts.com, после укороченной горизонтальной линии, этот номер повторяется, и за ним следуют библиографические сведения об источнике. Зачастую требуется, также указание  номера www.ukrtelecom.ua цитируемой страницы www.mysi.com.';
+//$reg2 = '#www\.([a-z0-9_-]+)\.([a-z]){2,4}#';
+$reg2 = '#www\.([a-z0-9]+)\.([a-z]){2,4}#';
+//$str3 = 'www.mts.com';
+  //preg_match($reg2, $str3, $m);
+ //print_r($m);
+
+
+//echo preg_replace($reg2, 'Ссылки запрещены', $str2);
+if (isset($_POST['submit'])){
+    $ustr = $_POST['ext1']; 
+    $ureg = '#^([a-z0-9_-]{3,16})$#i';    
+    
+    if (preg_match($ureg, $ustr)){
+        echo 'Password valid!!';
+    } else {
+        echo 'BAD Password!!';    
+    }
+
+}
+?>
+
+<form action="" method="post">
+                    <input type="text" name="ext1" />
+                    <input type="submit" name="submit"/>
+                </form>
