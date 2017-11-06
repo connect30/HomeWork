c78583427a40cedfe205f1645566d2815c40203d
connect30 <avolovodovskii@gmail.com>
2017-11-06 16:52:30 +0200

    06.11.2017_HW_11-13

--- function_reg1.php
+++ function_reg1.php
@@ -1,4 +1,23 @@
 <?php
+$reg = '#^\w...c$#i';
+$str = '57abc';
+ //echo preg_match($reg, $str, $m);
+ //print_r($m);
+ 
+ 
+ $str1 = date('Y.m.d H:i:s');
+ 
+ $reg1 = '#^(\d{4})\.(\d{2})\.(\d{2})\s(\d{2})\W(\d{2})\W(\d{2,})$#';
+ 
+ preg_match($reg1, $str1, $m);
+ foreach ($m as $value) {
+     echo '<br />'.$value;
+    
+}
+     
+// preg_match('@^(?:http://)?([^/]+)@',
+//    "http://www.php.net/index.html", $matches);
+//// echo $matches[1];
 $reg = '#^\w...[c$]#';
 $str = '57abd';
  echo preg_match($reg, $str, $m);
@@ -7,4 +26,3 @@ $str = '57abd';
  preg_match('@^(?:http://)?([^/]+)@',
     "http://www.php.net/index.html", $matches);
 // echo $matches[1];
-
